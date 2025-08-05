7d16bf02e48105ec379922c7a6b7ed7417ec3f7a
Rohith292 <rohk29@gmail.com>
2025-08-05 19:09:16 +0530

    updated frontedn vercel url

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173"||"https://family-map-flax.vercel.app",
     credentials: true,
   })
 );
